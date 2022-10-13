e96dad11d85fffbf9a09202a59b8f81152cf19bd
anquavious <agrant022@gmail.com>
2022-10-13 18:29:44 -0400

    added workout api

--- public/js/workout.js
+++ public/js/workout.js
@@ -1,13 +1,41 @@
-export const workOut = const options = {
+ const workout = {
     method: 'GET',
-    url: 'https://www.googleapis.com/fitness/v1/resourcePath?parameters',
     headers: {
-
-    }
+     'Accept': 'application/json',
+     'Content-Type':'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+  "dataStreamName": "MyDataSource",
+  "type": "derived",
+  "application": {
+    "detailsUrl": "http://example.com",
+    "name": "Foo Example App",
+    "version": "1"
+  },
+  "dataType": {
+    "field": [
+      {
+        "name": "steps",
+        "format": "integer"
+      }
+    ],
+    "name": "com.google.step_count.delta"
+  },
+  "device": {
+    "manufacturer": "Example Manufacturer",
+    "model": "ExampleTablet",
+    "type": "tablet",
+    "uid": "1000001",
+    "version": "1"
+  }
+})
 }
-    export const fetchData = async (url, options) => {
+    const url = process.env.GOOGLE_fit_url
+    const fetchData = async (url, options) => {
         const response = await fetch(url, options)
         const data = await response.json();
 
     return data;
 }
+
+module.exports = {workout, fetchData};
