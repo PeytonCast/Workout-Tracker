bab436d6a8a02f649f5ecaed144b2d825a0d85ab
Kianna Reid <kiannareid@yahoo.com>
2022-10-18 19:52:51 -0400

    updated workout, goals, and nutrition page

--- public/js/home.js
+++ public/js/home.js
@@ -1,36 +1,33 @@
-
 function renderPrograms() {
-    document.location.replace('/programs')
+  document.location.replace("/programs");
 }
-function renderChallenges() {
-    document.location.replace('/challenges')
-}
+
 function renderNutrition() {
-    document.location.replace('/nutrition')
+  document.location.replace("/nutrition");
 }
 function renderWorkouts() {
-    document.location.replace('/workouts')
+  document.location.replace("/workouts");
 }
 function renderSignup() {
-    document.location.replace('signup')
+  document.location.replace("signup");
 }
 
 document
-.getElementById('renderProgram')
-.addEventListener("click", renderPrograms)
-
-// document
-// .getElementById('render-signup')
-// .addEventListener("click", renderSignup)
+  .getElementById("renderProgram")
+  .addEventListener("click", renderPrograms);
 
 document
-.getElementById('renderChallenges')
-.addEventListener("click", renderChallenges)
+  .getElementById("render-signup")
+  .addEventListener("click", renderSignup);
 
 document
-.getElementById('renderNutrition')
-.addEventListener("click", renderNutrition)
+  .getElementById("renderChallenges")
+  .addEventListener("click", renderChallenges);
 
 document
-.getElementById('renderWorkouts')
-.addEventListener("click", renderWorkouts)
+  .getElementById("renderNutrition")
+  .addEventListener("click", renderNutrition);
+
+document
+  .getElementById("renderWorkouts")
+  .addEventListener("click", renderWorkouts);
